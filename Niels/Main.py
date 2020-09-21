23b5954000970572dfa7c4e72d91b4a514231483
Niels van der Vegt <nielsvdv1996@gmail.com>
2020-09-21 15:22:44 +0200

    Revert "Object oriented calculation"
    This reverts commit 52839667e9bed738da93ec7c408d7cec03f337d3.

--- Niels/Main.py
+++ Niels/Main.py
@@ -1,23 +1,12 @@
 """
 Main.py
 This is the main file where all parameters are set. The program should be started from this file.
-
-@author: nvandervegt
 """
 
 # Import own classes / modules
-import Other.GenericFunctions as f;
-import Modules.Uplift as Uplift;
-import Modules.Heave as Heave;
-#etc...
+import Modules.Uplift as Uplift
 
 
 # Run calculations
-# Uplift
-uplift = Uplift.Uplift();
-targetBetaUplift = f.ProbabilityToReliabilityIndex(1.30*10**-5)
-uplift.SetBetaReq(targetBetaUplift);
-uplift.DeterministicCalculation();
-
-# Heave
-heave = Heave.Heave();
+uplift = Uplift.Uplift()
+uplift.DeterministicCalculation()
